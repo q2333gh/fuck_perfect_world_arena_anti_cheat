896fe4a83fddb66b52b4ec50b3e6714614488900
Your Namebtwl <you@example.com>
2024-10-20 10:11:52 +0800

    add disable auto start feature

--- stop_shit_area_driver.py
+++ stop_shit_area_driver.py
@@ -1,10 +1,12 @@
+import subprocess
 import win32service
 import win32serviceutil
 import ctypes
 import sys
 import os
 
-def is_driver_installed(driver_name):
+
+def driver_installed_status(driver_name):
     try:
         win32serviceutil.QueryServiceStatus(driver_name)
         return True
@@ -28,6 +30,7 @@ def stop_driver(driver_name):
     except Exception as e:
         print(f"Failed to stop the driver '{driver_name}'. Error: {str(e)}")
         return False
+
 
 def request_uac_or_exit():
     username = os.getlogin()
@@ -55,21 +58,62 @@ def request_uac_or_exit():
             input("Press Enter to exit...")
 
 
-# pyinstaller --onefile stop_shit_area_driver.py
+def service_auto_statart_status(service_name):
+    try:
+        service_manager = win32service.OpenSCManager(
+            None, None, win32service.SC_MANAGER_ALL_ACCESS
+        )
+        service = win32service.OpenService(
+            service_manager, service_name, win32service.SERVICE_QUERY_CONFIG
+        )
+        config = win32service.QueryServiceConfig(service)
 
+        if config[1] == win32service.SERVICE_AUTO_START:
+            print(f"The service '{service_name}' is set to start automatically.")
+            return True
+        else:
+            print(f"The service '{service_name}' is not set to start automatically.")
+            return False
+    except Exception as e:
+        print(f"Failed to query the service '{service_name}'. Error: {str(e)}")
+        return False
+
+
+def disable_auto_start(driver_name):
+    try:
+        print("service is auto start,try to disable it ")
+        subprocess.run(
+            [
+                "powershell",
+                "-Command",
+                f"Set-Service -Name {driver_name} -StartupType Disabled",
+            ],
+            check=True,
+        )
+        print(f"Successfully disabled auto-start for the driver '{driver_name}'.")
+        return True
+    except subprocess.CalledProcessError as e:
+        print(
+            f"Failed to disable auto-start for the driver '{driver_name}'. Error: {str(e)}"
+        )
+
+
+# pyinstaller --onefile stop_shit_area_driver.py
 if __name__ == "__main__":
     # MessagingService_5e93b  C:\Windows\system32\svchost.exe -k UnistackSvcGroup  WAHTS  -k UnistackSvcGroup
-    print("INFO: 完美对战反作弊有几率造成蓝屏： anticheat driver with run(trigger) everytime when login into arena.exe\n     安装驱动然后调用他自己的DLL， 扫盘， 扫内存，扫进程，干扰Kernel")
+    print(
+        "INFO: 完美对战反作弊有几率造成蓝屏： anticheat driver with run(trigger) everytime when login into arena.exe\n     安装驱动然后调用他自己的DLL， 扫盘， 扫内存，扫进程，干扰Kernel"
+    )
 
     driver_name = "MessageTransfer"
 
-    if not is_driver_installed(driver_name):
+    if not driver_installed_status(driver_name):
         print(f"The driver '{driver_name}' is not installed.")
     else:
         print(f"INFO: The driver '{driver_name}' is installed.")
+        # print(service_auto_statart_status(driver_name))
         if is_driver_running(driver_name):
             print(f"The driver '{driver_name}' is currently running.")
-
             user_input = input("Do you want to stop the driver? (y/n): ")
             if user_input.lower() == "y":
                 request_uac_or_exit()
@@ -78,5 +122,11 @@ if __name__ == "__main__":
                 print("WARNING: Driver stop operation aborted by user.")
         else:
             print(f"GOOD: The driver '{driver_name}.sys' is not running.")
-    
+        if service_auto_statart_status(driver_name):
+            disable_auto_start(driver_name)
+            print(
+                f"After disable auto start: service_auto_statart_status is: "
+                + str(service_auto_statart_status(driver_name))
+            )
+
     input("Press Enter to exit...")
